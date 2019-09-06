66792e19ab4222f4fbc441c832df66b5bfe25b4d
Sowmya Kumar <sowmyak@unr.edu>
2019-09-05 22:05:30 -0700

    rounding formatting

---
+++ rounding.py
@@ -0,0 +1,13 @@
+# list numbers from 0 to 10 divide each by 3
+# print result as 2 decimal places
+
+# round()
+divisor = 3
+decimal_places = 2
+for number in range(0, 11):
+    #print ("{}, round(number/divisor, .format(decimial_places)")
+    
+   print( " This is my rounded result {0:.2f} also this {1:03d} ".format(number/divisor, 5))
+  
+    
+    
